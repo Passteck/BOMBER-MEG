73ab9e67c3b66f327f2779b5366e588293fd396d
Passteck <passteckh@gmail.com>
2025-06-17 15:41:23 +0200

    Projet Bomberman Maj: Limitation des déplacement dans la cage

--- Readme.md
+++ Readme.md
@@ -1,10 +1,14 @@
 # BOMBERMAN
+
 > Bomberman
+
 ## Fonctionnalité à developper
 
 - [ ] le terrain
 - [ ] les obstacles (fixe ou random)
-- [ ] le joueur (déplacement flèches directionnelles)
+- [ ] le joueur
+  - [x] déplacement flèches directionnelles
+  - [ ] limité les déplacement dans la fenêtre
 - [ ] les ennemis (déplacement aléatoire)
 - [ ] bombe
 - [ ] collision (obstacle, joueur, ennemis)
--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let sprite = document.querySelector(".sprite");
 let moveBy = 20;
 
 window.addEventListener("load", () => {
-  sprite.style.position = "absolute";
+  sprite.style.position = "relative";
   sprite.style.left = 0;
   sprite.style.top = 0;
 });
@@ -14,16 +14,38 @@ window.addEventListener("load", () => {
 window.addEventListener("keyup", (e) => {
   switch (e.key) {
     case "ArrowLeft":
-      sprite.style.left = parseInt(sprite.style.left) - moveBy + "px";
+      if (parseInt(sprite.style.left) == 0){
+        sprite.style.top = parseInt(sprite.style.left) = 0 + "px";
+      }
+      else {
+        sprite.style.left = parseInt(sprite.style.left) - moveBy + "px";
+      }
       break;
     case "ArrowRight":
-      sprite.style.left = parseInt(sprite.style.left) + moveBy + "px";
+      if (parseInt(sprite.style.left) == 680){
+        sprite.style.top = parseInt(sprite.style.left) = 0 + "px";
+      }
+      else {
+        sprite.style.left = parseInt(sprite.style.left) + moveBy + "px";
+      }
       break;
     case "ArrowUp":
-      sprite.style.top = parseInt(sprite.style.top) - moveBy + "px";
+      if (parseInt(sprite.style.top) == 0){
+        sprite.style.top = parseInt(sprite.style.top) = 700 + "px";
+      }
+      else {
+        sprite.style.top = parseInt(sprite.style.top) - moveBy + "px";
+      }
       break;
     case "ArrowDown":
-      sprite.style.top = parseInt(sprite.style.top) + moveBy + "px";
+      if (parseInt(sprite.style.top) == 680){
+        sprite.style.top = parseInt(sprite.style.top) = 0 + "px";
+      }
+      else {
+        sprite.style.top = parseInt(sprite.style.top) + moveBy + "px";
+      }
       break;
   }
 });
+
+
